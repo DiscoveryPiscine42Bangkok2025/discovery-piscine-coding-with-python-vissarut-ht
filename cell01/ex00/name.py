e7194decaa7e1a8bad90150537406b2e76451796
vissarut-ht <65070210@kmitl.ac.th>
2025-09-15 14:29:40 +0700

    update repo

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Vissarut"
+last_name = "Hongthong"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Vissarut"
+last_name = "Hongthong"
+whole_name = first_name + " " + last_name
+print(whole_name)
